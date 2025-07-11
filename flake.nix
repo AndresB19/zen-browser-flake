5b71173828d61d9445acd9860cdc960500ca4f46
Andres Bracho-Valles <97624614+AndresB19@users.noreply.github.com>
2025-07-11 18:00:28 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -8,17 +8,11 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.0.1-a.6";
+      version = "1.14.4b";
       downloadUrl = {
-        "specific" = {
-	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-	  sha256 = "sha256:0jkzdrsd1qdw3pwdafnl5xb061vryxzgwmvp1a6ghdwgl2dm2fcz";
+	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
+	  sha256 = "sha256:eb6b454400629de99657d6745f93295c82372478d65c8a32f10b5e8b555ab0d5";
 	};
-	"generic" = {
-	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-generic.tar.bz2";
-	  sha256 = "sha256:17c1ayxjdn8c28c5xvj3f94zjyiiwn8fihm3nq440b9dhkg01qcz";
-	};
-      };
 
       pkgs = import nixpkgs {
         inherit system;
